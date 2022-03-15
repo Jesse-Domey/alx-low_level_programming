c04cb585434bccab4056956c3f0ce89a73851c74
Jesse-Domey <99664883+Jesse-Domey@users.noreply.github.com>
2022-03-15 20:20:39 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char _putchar(void);
+char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,4 +15,4 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* MAIN_H */
+#endif /* MAIN */
